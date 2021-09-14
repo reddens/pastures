aa9d06699f05018711baf760ef69983180cb457c
CYril <cyrilokworu@protonmail.com>
2021-09-14 13:55:26 +0100

    update

--- db-connect.php
+++ db-connect.php
@@ -1,8 +1,8 @@
 <?php   
-   $dbhost = 'localhost';
-   $dbuser = 'bluefrog';
-   $dbpass = 'brokken';
+   $dbhost = 'uyu7j8yohcwo35j3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
+   $dbuser = '	e0umm0lwyh0x1o5k';
+   $dbpass = 'dpimmbrn7s5sh8m9';
    
    $conn= mysqli_connect("$dbhost","$dbuser","$dbpass") or die ("could not connect to mysql");
-   mysqli_select_db($conn, "bluefrog");
+   mysqli_select_db($conn, "tly8vcabxrsjijdk");
 ?>
